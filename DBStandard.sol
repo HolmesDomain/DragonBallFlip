fa509cecd61fa11792ed8dffefd190b0bfacfed0
Andrew J. Holmes <2pock72@gmail.com>
2018-08-14 14:58:09 -0400

    Create DBStandard.sol

---
+++ DBStandard.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.19;
+
+contract DBstandard {
+    
+    uint256[] iconSet = new uint256[599];
+    
+    function roll() {
+        //int = iconSet[x];
+        
+    }
+    
+    function trade() {
+        
+    }
+}
